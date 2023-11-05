93d8c567c4df47f393f44ed63927fe30712a3137
nina <ninabu16@gmail.com>
2023-11-05 15:20:59 +0100

    Initial project setup
    To finalize the setup, you must create database called 'med-equip' in pgAdmin before running the app.

---
+++ med_equipment/src/main/java/com/isa/med_equipment/MedEquipmentApplication.java
@@ -0,0 +1,14 @@
+package com.isa.med_equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedEquipmentApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ med_equipment/src/test/java/com/isa/med_equipment/MedEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.med_equipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedEquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
