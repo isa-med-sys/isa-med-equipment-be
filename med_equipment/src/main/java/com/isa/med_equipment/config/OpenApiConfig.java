a2404d2885783a1c995b2135355d284f1c3d5682
Anastasija <anastasijamarsenic@gmail.com>
2023-11-12 18:22:36 +0100

    feature: authorization using jwt token

---
+++ med_equipment/src/main/java/com/isa/med_equipment/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package com.isa.med_equipment.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Medical equipment",
+                        email = "medicalequipment753@gmail.com",
+                        url = " " // url for the website
+                ),
+                description = "OpenApi documentation",
+                title = "OpenApi documentation",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+}
--- med_equipment/src/main/java/com/isa/med_equipment/config/SecurityConfiguration.java
+++ med_equipment/src/main/java/com/isa/med_equipment/config/SecurityConfiguration.java
@@ -1,17 +1,20 @@
 package com.isa.med_equipment.config;
 
 import com.isa.med_equipment.auth.JwtAuthenticationFilter;
-
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import lombok.SneakyThrows;
+import static com.isa.med_equipment.enums.Permission.ADMIN_READ;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +22,18 @@ public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+
+    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"};
 
     public SecurityConfiguration(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
         this.jwtAuthFilter = jwtAuthFilter;
@@ -29,19 +44,20 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
-                .csrf()
-                .disable().authorizeHttpRequests()
-                .requestMatchers("/api/users/**").permitAll()
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui.html/**", "/swagger-ui/index.html", "swagger-ui/**",  "/swagger-ui/swagger-initializer.js", "/swagger-resources/**", "/webjars/**").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req
+                                .requestMatchers(WHITE_LIST_URL).permitAll()
+                                .requestMatchers(POST, "/api/users/register", "/api/users/login").permitAll()
+                                .requestMatchers(GET, "/api/users/confirm-account").permitAll()
+                                .requestMatchers(GET, "/api/users/getAll").hasAnyAuthority(ADMIN_READ.name())
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
         return http.build();
     }
 }
